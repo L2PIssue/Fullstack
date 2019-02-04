bf75f3c255ae201413bac59a2b8d091eb26c2761
Miiu <miia.ramo@helsinki.fi>
2019-02-04 19:41:13 +0200

    still missing the search

--- part_2/phonebook/src/index.js
+++ part_2/phonebook/src/index.js
@@ -2,12 +2,76 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import React, { useState } from 'react'
 
+const Title = () => {
+  return (
+    <h1>Puhelinluettelo</h1>
+  );
+}
+
+const FilterForm = (props) => {
+  return (
+    <div>
+      Etsi:
+        <input 
+            value={props.search}
+            onChange={props.handleSearch}
+        />
+    </div>
+  );
+}
+
+const PersonForm = (props) => {
+  return (
+    <div>
+      <h2>Lisää uusi</h2>
+      <form onSubmit={props.addNew}>
+        <div>
+          nimi: 
+          <input 
+            value={props.newName}
+            onChange={props.handleNewName}
+          />
+        </div>
+        <div>
+          puhelinnumero:
+        <input 
+            value={props.newNumber}
+            onChange={props.handleNewNumber}
+          />
+        </div>
+        <div>
+          <button type='submit'>lisää</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+const People = (props) => {
+  const personList = props.persons.map((person) =>
+    <li key={person.name}>
+      {person.name} {person.number}
+    </li>
+  );
+  return (
+    <div>
+      <h2>Numerot</h2>
+      <ul>{personList}</ul>
+    </div>
+  )
+}
+
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '050-1234567' }
+    { name: 'Some Name', number: '050-1234567' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ search, setNewSearch ] = useState('')
+  
+  const handleSearch = (event) => {   
+    setNewSearch(event.target.value)
+  }
 
   const handleNewName = (event) => {   
     setNewName(event.target.value)  
@@ -31,36 +95,15 @@ const App = () => {
     setNewName('')
   }
 
-  const personList = persons.map((person) =>
-    <li key={person.name}>
-      {person.name} {person.number}
-    </li>
-  );
-
   return (
     <div>
-      <h2>Puhelinluettelo</h2>
-      <form onSubmit={addNew}>
-        <div>
-          nimi: 
-          <input 
-            value={newName}
-            onChange={handleNewName}
-          />
-        </div>
-        <div>
-          puhelinnumero:
-        <input 
-            value={newNumber}
-            onChange={handleNewNumber}
-          />
-        </div>
-        <div>
-          <button type="submit">lisää</button>
-        </div>
-      </form>
-      <h2>Numerot</h2>
-      <ul>{personList}</ul>
+      <Title />
+      <FilterForm search={search} setNewSearch={setNewSearch} />
+      <PersonForm newName={newName} 
+                  newNumber={newNumber} 
+                  handleNewName={handleNewName} 
+                  handleNewNumber={handleNewNumber} />
+      <People persons={persons}/>
     </div>
   )
 
